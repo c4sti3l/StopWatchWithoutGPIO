bc05aac4d7be7d32c25a74d9dd94cdb1d378864f
c4sti3l <chennenberg@gmx.de>
2019-05-24 01:13:06 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         MainController controller = new MainController();
+        System.out.println("lol");
         controller.init();
 
 //        JFrame frame = new JFrame();
